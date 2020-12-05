adb3dcbd2d00c58bd6946d012c9ad581df325c98
Laytan Laats <Laytanlaats@hotmail.com>
2020-12-05 02:34:17 +0100

    fix verticalAccuracy field and use enum for LocationProvider

--- location.go
+++ location.go
@@ -2,7 +2,33 @@ package termux
 
 import (
 	"encoding/json"
+	"fmt"
 	"os/exec"
+)
+
+// LocationProvider is the provider of a location
+type LocationProvider uint
+
+func (l LocationProvider) String() string {
+	return []string{"gps", "network", "passive"}[l]
+}
+
+// UnmarshalJSON turns the json data into one of the enum types
+func (l *LocationProvider) UnmarshalJSON(data []byte) error {
+	for i, provider := range []string{"gps", "network", "passive"} {
+		if toEnumStr(data) == provider {
+			*l = LocationProvider(i)
+			return nil
+		}
+	}
+	return fmt.Errorf("No provider enum type for %s", string(data))
+}
+
+// All possible location providers
+const (
+	LocationProviderGPS LocationProvider = iota
+	LocationProviderNetwork
+	LocationProviderPassive
 )
 
 // LocationInfo is the information about the device
@@ -11,13 +37,12 @@ type LocationInfo struct {
 	Longitude        float64
 	Altitude         float64
 	Accuracy         float64
-	VerticalAccuracy float64
+	VerticalAccuracy float64 `json:"vertical_accuracy"`
 	Bearing          float64
 	Speed            float64
 	// Time on which the speed is calculated
 	ElapsedMS uint
-	// gps, wifi etc
-	Provider string
+	Provider  LocationProvider
 }
 
 // Location returns the device's location info (requires permission)
